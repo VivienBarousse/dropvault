928ecfbb87dc612d36cf712c4db126c4b2a7344d
Vivien Barousse <barousse.vivien@gmail.com>
2011-06-28 12:40:28 +0100

    Corrected typo in android app unit tests

--- dropvault-android/src/test/java/com/aperigeek/dropvault/android/service/HashServiceTest.java
+++ dropvault-android/src/test/java/com/aperigeek/dropvault/android/service/HashServiceTest.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with dropvault.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.aperigeel.dropvault.android.service;
+package com.aperigeek.dropvault.android.service;
 
 import com.aperigeek.dropvault.android.service.HashService;
 import junit.framework.TestCase;
