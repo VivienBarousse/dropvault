eab68f468cbaef9bcda72811e7cbad92b4c5899f
Vivien Barousse <barousse.vivien@gmail.com>
2011-07-01 11:08:51 +0100

    Using new layout for receiving files.
    This new layout allows to chose the file name and confirm before saving the
    file.

--- dropvault-android/src/main/java/com/aperigeek/dropvault/android/activity/ReceiveFileActivity.java
+++ dropvault-android/src/main/java/com/aperigeek/dropvault/android/activity/ReceiveFileActivity.java
@@ -22,7 +22,12 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import com.aperigeek.dropvault.R;
 import com.aperigeek.dropvault.android.FilesListActivity;
 import com.aperigeek.dropvault.android.service.FilesService;
 import java.io.IOException;
@@ -33,13 +38,19 @@ import java.util.logging.Logger;
  *
  * @author Vivien Barousse
  */
-public class ReceiveFileActivity extends Activity {
+public class ReceiveFileActivity extends Activity implements OnClickListener {
     
     private static final Logger logger = Logger.getLogger(ReceiveFileActivity.class.getName());
 
     private SharedPreferences prefs;
     
     private FilesService service;
+    
+    private Uri uri;
+    
+    private String mime;
+    
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,9 +71,24 @@ public class ReceiveFileActivity extends Activity {
         Intent intent = getIntent();
         if (Intent.ACTION_SEND.equals(intent.getAction())) {
             Bundle extras = intent.getExtras();
-            Uri uri = extras.getParcelable(Intent.EXTRA_STREAM);
-            String mime = intent.getType();
-            String name = uri.getLastPathSegment();
+            uri = extras.getParcelable(Intent.EXTRA_STREAM);
+            mime = intent.getType();
+            name = uri.getLastPathSegment();
+        }
+        
+        setContentView(R.layout.receive_file_activity);
+        
+        Button okButton = (Button) findViewById(R.id.receive_file_ok);
+        okButton.setOnClickListener(this);
+        
+        EditText nameText = (EditText) findViewById(R.id.receive_file_name);
+        nameText.setText(name);
+    }
+    
+    public void onClick(View view) {
+        if (view.getId() == R.id.receive_file_ok) {
+            EditText nameText = (EditText) findViewById(R.id.receive_file_name);
+            name = nameText.getText().toString();
             
             try {
                 service.importFile(uri, mime, name);
@@ -71,8 +97,8 @@ public class ReceiveFileActivity extends Activity {
                 Toast toast = Toast.makeText(this, "Unable to import file", 10);
                 toast.show();
             }
+            startActivity(new Intent(this, FilesListActivity.class));
         }
-        startActivity(new Intent(this, FilesListActivity.class));
     }
 
     @Override
